b344c23b1a1253d4e4665b5ec3b8f3d0d9074948
Sanjib Ahmad <sanjib@sanjib.org>
2015-04-09 04:14:30 +0600

    Initial Commit

---
+++ Is Headphone Plugged In/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Is Headphone Plugged In
+//
+//  Created by Sanjib Ahmad on 4/9/15.
+//  Copyright (c) 2015 Object Coder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
